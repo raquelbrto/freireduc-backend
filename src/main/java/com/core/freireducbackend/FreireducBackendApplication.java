9e469db8e708ea2f99b666b19ca4e996420b4a2a
Raquel Brito <raquelbrito662@gmail.com>
2023-09-07 08:10:03 -0300

    Initial commit

---
+++ src/main/java/com/core/freireducbackend/FreireducBackendApplication.java
@@ -0,0 +1,13 @@
+package com.core.freireducbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreireducBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreireducBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/freireducbackend/FreireducBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.freireducbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreireducBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
